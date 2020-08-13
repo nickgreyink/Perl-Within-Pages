015a9d19be87f9b42ad5f1c3b740cce8e19921d2
Nick <exsatus@live.com>
2020-08-13 14:00:35 +0000

    Even more comments.

--- pwp.pl
+++ pwp.pl
@@ -27,37 +27,68 @@ sub inline_print{
 # place the text of the page in variable
 # $page_text
 	my $page_text = $_[0];
+# replaces the first instance for <pwp with nothing
+# othereise you would not be able to have text
+# before you made the first <pwp
 	$page_text =~ s/<pwp//;
+# replaces the last pwp> with nothing
+# or you couldn't have text after your last
+# pwp>
 	$page_text =~ s/pwp>$//;
+# replaces all the other <pwp's with "; to
+# end the perl print statement
 	$page_text =~ s/<pwp/";/g;
+# replaces all the other pwp>'s with
+# print" to start a perl print statement
+# to include html and othet text
 	$page_text =~ s/pwp>/print"/g;
 	return $page_text;
 }
 
-
+# just initializes a variable to hold
+# the string from the loaded page
 my $page_string = "";
 
+# initializes a varaible to hold the
+# name of the file being loaded
 my $filename = "";
 
+# checks if there is a file called
+# imdex.pl in the folder being accessed
+# by pwp
 if(-e "index.pl"){
 	$filename = "index.pl";
-}elsif(-e "default.pl"){
+}
+# checks for default.pl page
+elsif(-e "default.pl"){
 	$filename = "default.pl";
-}elsif ($ARGV[0]){
+}
+# this will pull the file given to
+# pwp.pl through the command line
+elsif ($ARGV[0]){
 	$filename = $ARGV[0];
-}else{
+}
+# if there is no file for the index
+# then this will be displayed
+else{
 	print "Index file not found.";
 	exit;
 }
 
+# checking if the file to be displayed
+# acctually exists
 if ($filename ne '') {
+	# open the file
 	open(my $fh, '<:encoding(UTF-8)', $filename)
 	or die "Could not open file '$filename' $!";
-		
+
+# read each line of the page
+# and add each row to a variable
 	while (my $row = <$fh>) {
 		chomp $row;
 		$page_string .= $row;
 	}
 }
 
+# eval all the string as perl code
 eval inline_print($page_string);
